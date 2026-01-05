e7e4872c372cb462a2665eee8215eac1eab78cb1
ThiagoFranca <francasantos.t@gmail.com>
2026-01-04 21:07:30 -0500

    fetaure: navbar done com as iniciais do login espacada e configurada totaljmente

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatDividerModule } from "@angular/material/divider";
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    MatToolbarModule, 
+    MatButtonModule, 
+    MatIconModule, 
+    MatMenuModule,
+    MatDividerModule
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
 
+export class NavbarComponent {
+  userName = 'Thiago Franca';
+
+  get initials(): string {
+    return this.userName.split(' ').map(n => n[0]).join('').toUpperCase();
+  }
 }
